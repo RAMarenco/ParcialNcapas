e463182b952e2b91bd0faa520eb7e7aace0df8d6
RAMarenco <00080121@uca.edu.sv>
2024-06-05 21:13:25 -0600

    Init

---
+++ Parcial2/src/test/java/org/happybaras/parcial2/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.happybaras.parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
